c846715b3c9c4c461bf89898c270659f86182cb2
Durjoy Acharya <62555348+DurjoyAcharya@users.noreply.github.com>
2020-10-03 22:47:27 +0600

    Create  FormatConverter.cs

---
+++ FormatConverter.cs
@@ -0,0 +1,27 @@
+public class FormatConverter
+{
+    public static void Main(string[] args)
+    {
+        string lineInput;
+        while ((lineInput = Console.ReadLine()) != null)
+        {
+            string[] fields = lineInput.Split(new char[] {'\t'});
+            bool isFirstField = true;
+            foreach (var item in fields)
+            {
+                if (isFirstField)
+                    isFirstField = false;
+                else
+                    Console.Write(',');
+
+                // If the field represents a boolean, replace with a numeric representation.
+                bool itemBool;
+                if (Boolean.TryParse(item, out itemBool))
+                    Console.Write(Convert.ToByte(itemBool));
+                else
+                    Console.Write(item);
+            }
+            Console.WriteLine();
+        }
+    }
+}
